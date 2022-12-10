8be92d70570e6ada5a9ab50a04d075ca438ee283
Mathijs Kadijk <mkadijk@gmail.com>
2022-12-10 20:24:42 +0100

    Day 10 tweaks

--- 2022/Sources/AoC2022/Day10.swift
+++ 2022/Sources/AoC2022/Day10.swift
@@ -10,23 +10,22 @@ import Foundation
 func day10(filePath: String) -> Int {
     let xValues = LineReader(path: filePath)
         .flatMap { [0, Int($0.split(separator: " ").last!)] }
-        .compactMap { $0 }
+        .compactMap { $0 } // Filter nils from the noop instructions
         .reduce(into: [1]) { acc, xChange in
             acc += [acc.last! + xChange]
         }
     
     // Part 2
     let crtOutput = xValues
-        .map { ($0-1)...($0+1) }
         .enumerated()
-        .map { (pixelPosition, sprite) in
-            sprite.contains(pixelPosition % 40) ? "#" : "."
+        .map { (pixelPosition, xVal) in
+            ((xVal-1)...(xVal+1)).contains(pixelPosition % 40) ? "#" : "."
         }
         .chunked(size: 40)
         .map { String($0) }
         .joined(separator: "\n")
     print(crtOutput)
-    
+
     // Part 1
     return stride(from: 20, to: xValues.count, by: 40)
         .reduce(0) { partialResult, cycleNumber in
